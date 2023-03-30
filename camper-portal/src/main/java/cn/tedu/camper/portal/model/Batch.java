b76b63d8d6b63ca9f08127586b0750e6fc39e068
josh <josh19910507@gmail.com>
2023-03-30 11:49:01 +0800

    camper

---
+++ camper-portal/src/main/java/cn/tedu/camper/portal/model/Batch.java
@@ -0,0 +1,10 @@
+package cn.tedu.camper.portal.model;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+
+public class Batch {
+}
